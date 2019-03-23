f647855df01640c7370a7bd221e33fb536f81262
olegmanzhay <oleg.manzhay@yandex.ru>
2019-03-23 17:10:28 +0500

    Написаны 1 и 2 дз.
    Выполено дз 1:
    1. Проверка на введение только целых чисел
    2. Нахождение минимального значения
    
    Выполено дз 2:
    1. Проверка на введение числа целого и с плавующей точкой
    2. С помощью таблицы ASCII выбраны варианты, определяющие число введено или нет

---
+++ Lesson_2/Homework/Homework/Program.cs
@@ -0,0 +1,71 @@
+﻿/*1. Написать метод, возвращающий минимальное из трех чисел.
+ Манжай Олег*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        /// <summary>
+        /// Поиск минимального значения из 3 элементов
+        /// </summary>
+        /// <param name="x">Первое значение</param>
+        /// <param name="y">Второе значение</param>
+        /// <param name="z">Третье значение</param>
+        /// <returns>Выводит минимальное значение</returns>
+        static int minNumber(int x, int y, int z)
+        {
+            int minValue;
+            if (x < y)
+            {
+                minValue = x;
+            }
+            else
+            {
+                minValue = y;
+            }
+
+            if (z < minValue)
+            {
+                minValue = z;
+            }
+
+            return minValue;
+        }
+        static void Main(string[] args)
+        {
+            string a, b, c;
+            int A, B, C;
+            Console.WriteLine("Добрый день! Это программа поиска минимального значения из 3 чисел");
+            do {
+                Console.Write("Введите a: ");
+                a = (Console.ReadLine());
+            }while(!int.TryParse(a, out A));// пока не введется значение Int32
+            do
+            {
+                Console.Write("Введите b: ");
+                a = (Console.ReadLine());
+            } while (!int.TryParse(a, out B));
+            do
+            {
+                Console.Write("Введите c: ");
+                a = (Console.ReadLine());
+            } while (!int.TryParse(a, out C));
+
+            if (A == B && B == C)
+            {
+                Console.WriteLine("Все числа равны");
+            }
+            else
+            {
+                Console.WriteLine($"Минимальное значение составляет: {minNumber(A, B, C)}");
+            }
+
+             Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_2/Homework/Homework2/Program.cs
@@ -0,0 +1,70 @@
+﻿/*2. Написать метод подсчета количества цифр числа.
+ Манжай Олег*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        /// <summary>
+        /// Находит количество цифр в числе
+        /// </summary>
+        /// <param name="figure">принямает на вход строку</param>
+        /// <returns>количество цифр</returns>
+        static int countDigits(string figure)
+        {
+            int count = 0; // счетчик цифр 
+            int notCount = 0;// счетчик не цифр
+            for (int i = 0; i < figure.Length; i++)
+            {
+                if (figure[i] >= 41 && figure[i] <= 57)//Если выпадает цифра -  увеличиваем счетчик (Таблица ASCII)
+                {
+                    switch (figure[i])
+                    {
+                        case (char)44://Если выпадает ',' - не будет увеличиваться счетчик цифр
+                            break;
+                        case (char)45://Если выпадает '-' - не будет увеличиваться счетчик цифр
+                            break;
+                        default:
+                            count++;
+                            break;
+                    }
+                }
+                else
+                {
+                    notCount++;
+                }
+            }
+            if (notCount > 0)
+            {       
+                return 0;
+            }
+            else
+            {   
+                return count;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string number;
+            Console.WriteLine("Здравствуйте, это программа подсчета цифр в цисле!");
+            Console.Write("Введите число: ");
+            number = Console.ReadLine();
+            
+            if(countDigits(number) == 0)
+            {
+                Console.WriteLine("Вы ввели не число");
+            }
+            else
+            {
+                Console.WriteLine($"Количество цифр в числе составляет: {countDigits(number)}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
